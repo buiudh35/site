020191e38975c630e0d9c224c608d511c9353ff9
buiudh35 <79838021+buiudh35@users.noreply.github.com>
2024-09-04 06:30:07 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
 
-// Função para rolagem suave ao clicar em links do menu
+// Rolagem suave ao clicar nos links do menu
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
