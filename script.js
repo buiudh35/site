05294f590937a0b2a78068fa46fa4631f1422d07
buiudh35 <79838021+buiudh35@users.noreply.github.com>
2024-09-04 21:16:35 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -25,3 +25,12 @@ nav.addEventListener('click', () => {
         nav.classList.remove('nav-open');
     }
 });
+
+
+
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('nav ul');
+
+navToggle.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+});
